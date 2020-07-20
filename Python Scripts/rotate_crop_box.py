93c3ec098ea5a3391b0b886e4c023fb19160d647
Jeremy Moore <alt.photosbyjeremy@gmail.com>
2020-07-20 11:53:21 -0400

    Update rotate_crop_box.py
    add batch Rotate

--- Python Scripts/rotate_crop_box.py
+++ Python Scripts/rotate_crop_box.py
@@ -15,18 +15,6 @@ if __name__ == '__main__':
     arg_parser.add_argument("--concatenate", required=False, help="Put anything here to add the inputted degrees to the current rotation value")
     args = vars(arg_parser.parse_args())
 
-    print(f'Rotate crop box {args["degrees"]} degrees {args["angle"]}.')
-
-    command = co.command_stub + [f'{co.tell_co} to set primary_variant to (get primary variant)', 'return primary_variant']
-    primary_variants_list = applescript.command_to_python_list(command)
-
-    primary_variant = co.Variant(primary_variants_list[0])
-
-    pre_rotation_list = co.get_adjustment_values(primary_variant.document, primary_variant.collection_id, primary_variant.id, 'rotation')
-    degrees_pre_rotated = pre_rotation_list[0]
-
-    print(f'Pre-rotation of {primary_variant.id}: {degrees_pre_rotated}')
-
     # set angle to lower case and get degrees as a float
     angle = args["angle"].lower()
     degrees = float(args["degrees"])
@@ -35,10 +23,26 @@ if __name__ == '__main__':
         # rotation should be negative
         degrees = -degrees
 
-    if args["concatenate"]:
-        degrees = float(degrees_pre_rotated) + degrees
 
-    post_rotation_list = co.set_adjustment_value(primary_variant.document, primary_variant.collection_id, primary_variant.id, 'rotation', str(degrees))
-    degrees_post_rotated = post_rotation_list[0]
+    # get list of selected variants to process
+    selected_variants_list = co.get_selected_variants()
+    number_of_selected_variants = len(selected_variants_list)
+    applescript.display_notification(f'Rotating {number_of_selected_variants} VARIANTS {degrees} {angle}')
 
-    print(f'Post-rotation of {primary_variant.id}: {degrees_post_rotated}')
+    for index, selected_variant in enumerate(selected_variants_list):
+        degrees_to_rotate = 0
+        variant = co.Variant(selected_variant)
+        pre_rotation_list = co.get_adjustment_values(variant.document, variant.collection_id, variant.id, 'rotation')
+        degrees_pre_rotated = pre_rotation_list[0]
+
+
+
+        if args["concatenate"]:
+            degrees_to_rotate = float(degrees_pre_rotated) + degrees
+        else:
+            degrees_to_rotate = degrees
+
+        post_rotation_list = co.set_adjustment_value(variant.document, variant.collection_id, variant.id, 'rotation', str(degrees_to_rotate))
+        degrees_post_rotated = post_rotation_list[0]
+
+    applescript.display_notification(f'Rotated {number_of_selected_variants} VARIANTS {angle}')
